01b75040801fe578038c77ecd282065e475f713e
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-05 13:25:32 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n%100;
+int n2=n1/10;
+
+if (n >99 && n<1000)
+{
+    Console.WriteLine(n2);
+}
+
+else
+{
+    Console.WriteLine("Введите ТРЕХЗНАЧНОЕ число! Это не трехзначное число!");
+}
