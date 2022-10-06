a0b815e34ca967f56be1f0c1f74b0072622ae364
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-06 16:24:35 +0300

    task13new

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите число, а явыведу трерью цифру");
 
 string str = Console.ReadLine();
 int m=str.Length;
@@ -15,7 +15,26 @@ double result = strDou%n2;
 double result2 = result/n;
 double result2M = Math.Round(result2);
 
-if (result2M>result2)
-    Console.WriteLine(result2M-1);
-else
-    Console.WriteLine(result2M);
+if (m<3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    if (result2M>result2)
+    {
+        Console.WriteLine(result2M-1);
+
+    }
+
+    else
+    {
+        Console.WriteLine(result2M);
+
+    }
+}
+
+
+
+    
+
