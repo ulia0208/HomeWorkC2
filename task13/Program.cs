e579de683826c7284c1e846167c7046865c80fb4
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-06 15:03:44 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+
+string str = Console.ReadLine();
+int m=str.Length;
+
+double n = Math.Pow(10,m-3);
+double n2 = Math.Pow(10,m-2);
+
+int nI=(int)n;
+int nI2=(int)n2;
+
+double strDou = Double.Parse(str);
+double result = strDou%n2;
+double result2 = result/n;
+double result2M = Math.Round(result2);
+
+if (result2M>result2)
+    Console.WriteLine(result2M-1);
+else
+    Console.WriteLine(result2M);
