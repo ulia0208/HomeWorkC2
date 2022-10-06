30b0d76f4df6587a20d469f760a519c8de5079e2
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-06 15:35:36 +0300

    task15

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Введите порядковый номер дня недели");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>0 && n<6)
+{
+    Console.WriteLine("Будний");
+}
+else if (n>5 && n<8)
+{
+    Console.WriteLine("Выходной");
+}
+else 
+{
+    Console.WriteLine("Нет такого дня недели");
+}
+
